3867e8fc351062b60437e8ea7ffc705e6bbc40ed
iespino <iespino69@gmail.com>
2018-09-26 23:33:35 -0500

    26-09-2018

--- getData.php
+++ getData.php
@@ -17,11 +17,27 @@ function getData($conexion)
                                    tcodes.id_submodulo = submodulos.id_submodulo
                  inner join modulos on
                             submodulos.id_modulo = modulos.id_modulo ";
+    
+    $query_tables = "SELECT tables.id_table,
+                            tables.table,
+                            tables.description_table,
+                            submodulos.submodulo,
+                            submodulos.descripcion_submodulo,
+                            modulos.modulo,
+                            modulos.descripcion                                 
+                       from tables
+                 inner join submodulos on
+                            tables.id_submodulo = submodulos.id_submodulo
+                 inner join modulos on
+                            submodulos.id_modulo = modulos.id_modulo ";
 
     $total_tcodes = mysql_query($query_tcodes);
+    $total_tables = mysql_query($query_tables);
 
     $tcodes = array();
+    $tables = array();
     $ind=0;
+    $ind2=0;
 
     while($rowT = mysql_fetch_array($total_tcodes)) 
          {
@@ -35,11 +51,24 @@ function getData($conexion)
            $tcodes[$ind] =  $data ;
            $ind++;
          }
+    
+    while($rowTa = mysql_fetch_array($total_tables)) 
+         {
+           $data_t['table'] = $rowTa['table'];
+           $data_t['table_descripcion'] = $rowTa['description_table'];
+           $data_t['submodulo'] = $rowTa['submodulo'];
+           $data_t['descripcion_submodulo'] = $rowT['descripcion_submodulo'];
+           $data_t['modulo'] = $rowTa['modulo'];
+           $data_t['modulo_descipcion'] = $rowTa['descripcion'];
+        
+           $tables[$ind] =  $data_t ;
+           $ind2++;
+         }
 
-         utf8_converter($tcodes);
+         utf8_converter($tcodes,$tables);
 }
 
-function utf8_converter($tcodes)
+function utf8_converter($tcodes,$tables)
 {
     array_walk_recursive($tcodes, function(&$item, $key)
     {
@@ -47,8 +76,16 @@ function utf8_converter($tcodes)
                 $item = utf8_encode($item);
         }
     });
+
+    array_walk_recursive($tables, function(&$item, $key)
+    {
+        if(!mb_detect_encoding($item, 'utf-8', true)){
+                $item = utf8_encode($item);
+        }
+    });
  
-    createJson($tcodes);
+ 
+    createJson($tcodes,$tables);
 }
 
 function createJson($tcodes)
@@ -57,7 +94,14 @@ function createJson($tcodes)
     $fh = fopen("tcodes.json", 'w');
     fwrite($fh, $jsonencoded);
     fclose($fh);
-    echo "Archivo JSON Creado";
+    echo "Archivo JSON Tcode Creado";
+
+    $jsonencoded = json_encode($tables,JSON_UNESCAPED_UNICODE);
+    $fh = fopen("tables.json", 'w');
+    fwrite($fh, $jsonencoded);
+    fclose($fh);
+    echo "Archivo JSON Tables Creado";
+
     updateRepository();
 }
 function updateRepository()
